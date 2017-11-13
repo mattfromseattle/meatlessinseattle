e3926a1a525a5ec51031aa2a32d83b7a80dde358
mattfromseattle <mdewey@deloitte.com>
2017-11-12 16:02:48 -0800

    Added a new page, worked on filtering more.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,34 +9,34 @@
  * Main module of the application.
  */
 angular
-  .module('meatlessinseattleApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        //controllerAs: 'main'
-      })
-      .when('/recipes', {
-        templateUrl: 'views/recipes.html',
-        controller: 'RecipesCtrl',
-        //controllerAs: 'recipes'
-      })
-      .when('/searchresults', {
-        templateUrl: 'views/searchresults.html',
-        controller: 'SearchResultsCtrl',
-        //controllerAs: 'searchResults'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('meatlessinseattleApp', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+                //controllerAs: 'main'
+            })
+            .when('/recipes', {
+                templateUrl: 'views/recipes.html',
+                controller: 'RecipesCtrl',
+                //controllerAs: 'recipes'
+            })
+            .when('/additionalInformation', {
+                templateUrl: 'views/additionalInformation.html',
+                controller: 'InformationCtrl',
+                //controllerAs: 'recipes'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
